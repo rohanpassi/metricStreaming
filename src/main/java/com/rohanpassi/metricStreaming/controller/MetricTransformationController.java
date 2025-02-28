f97724e9e3779c7514ef3e8e94fd245ab86c5078
Rohan Passi <rohanpassi94@gmail.com>
2025-02-28 14:31:17 -0800

    Remove unused imports from Controller

--- src/main/java/com/rohanpassi/metricStreaming/controller/MetricTransformationController.java
+++ src/main/java/com/rohanpassi/metricStreaming/controller/MetricTransformationController.java
@@ -1,7 +1,5 @@
 package com.rohanpassi.metricStreaming.controller;
 
-import com.rohanpassi.metricStreaming.dto.Transformation;
-import com.rohanpassi.metricStreaming.model.Metric;
 import com.rohanpassi.metricStreaming.service.MetricTransformationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,15 +7,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.rohanpassi.metricStreaming.request.TransformationRequest;
 
-import java.util.List;
-
 @RestController
 public class MetricTransformationController {
     @Autowired
     private MetricTransformationService metricTransformationService;
 
     @PostMapping("/transform")
-    public Object transformMetrics(@RequestBody TransformationRequest request) { //Updated
-        return metricTransformationService.transform(request.getMetrics(), request.getTransformation()); //updated
+    public Object transformMetrics(@RequestBody TransformationRequest request) {
+        return metricTransformationService.transform(request.getMetrics(), request.getTransformation());
     }
 }
